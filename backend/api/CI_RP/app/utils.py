ba4830c8f6e6ca46c6d522f60aa5733ebe28804e
Grancho <guilhermegranchopro@gmail.com>
2025-05-06 14:53:50 +0100

    Remove unused import for datetime in utils.py

--- backend/api/CI_RP/app/utils.py
+++ backend/api/CI_RP/app/utils.py
@@ -5,7 +5,6 @@ import pandas as pd
 import numpy as np
 import joblib
 import tensorflow as tf
-from datetime import datetime
 
 # Load secrets from env
 API_KEY = os.getenv("ELECTRICITYMAP_API_KEY")
